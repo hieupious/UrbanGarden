e5260295c5b7efd32d15e9a40ab4cc9288fcc9a1
Hieu Pious <hieupious@ymail.com>
2016-04-26 21:08:10 +0700

    new fixed

--- public/js/script.js
+++ public/js/script.js
@@ -34,52 +34,6 @@ $(window).load(function() {
 // });
 
 $(document).ready(function() {
-	function sliderPanelSize() {
-		$( ".fp-slidesNav li a span" ).css({
-				"height": $(window).height(),
-				"width": $(window).height()
-			});
-	}
-	$(window).on("load", sliderPanelSize);
-	$(window).on("resize", sliderPanelSize);
-
-	// Fullpage
-	$('#homepage_wrapper').fullpage({
-		anchors: ['main', 'our-products', 'outlet-store', 'showroom', 'premier', 'stories'],
-		navigation: true,
-		navigationPosition: 'right',
-		scrollOverflow: true,
-		slidesNavigation: true,
-		slidesNavPosition: "right",
-		controlArrows: true,
-		afterRender: function () {
-	        //on page load, start the slideshow
-	         slideTimeout = setInterval(function () {
-	                $.fn.fullpage.moveSlideRight();
-	            }, 8000);
-	    },
-	    onLeave: function (index, direction) {
-	        //after leaving section 1 (home) and going anywhere else, whether scrolling down to next section or clicking a nav link, this SHOULD stop the slideshow and allow you to navigate the site...but it does not
-	        if (index == '3') {
-	            console.log('on leaving the slideshow/section3');
-	            clearInterval(slideTimeout);
-	        }
-	   },
-		afterLoad: function(anchorLink, index){
-				if(index == 3){
-					$('.fadeInDown').addClass('animated');
-				}
-				if(index == 4){
-					$('.fadeInUp').addClass('animated');
-				}
-				if(index == 5){
-					$('.bounceInUp').addClass('animated');
-				}
-				if(index == 6){
-					$('.fadeInUp , .fadeInDown').addClass('animated');
-				}
-			}
-	});
 
 	//animated logo
 	$(".navbar-brand").hover(function () {
