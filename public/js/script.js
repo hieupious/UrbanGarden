1deb7ef7bc17d37a7be19878475f13d63e5015b3
Hoang Trung Hieu <hieupious@ymail.com>
2016-05-11 08:56:34 +0700

    move script and change data anchor

--- public/js/script.js
+++ public/js/script.js
@@ -7,32 +7,6 @@ $(window).load(function() {
 
 })
 
-// GET TRANSLATE VALUE
-// $(document).ready(function(){
-// 	function $m(id){
-// 	   return document.getElementById(id);
-// 	}
-//     $("#homepage_wrapper").scroll(function(){
-//         function getTransform(el) {
-// 				var transform = window.getComputedStyle(el, null).getPropertyValue('transform');
-// 				var results = transform.match(/matrix(?:(3d)\(-{0,1}\d+(?:, -{0,1}\d+)*(?:, (-{0,1}\d+))(?:, (-{0,1}\d+))(?:, (-{0,1}\d+)), -{0,1}\d+\)|\(-{0,1}\d+(?:, -{0,1}\d+)*(?:, (-{0,1}\d+))(?:, (-{0,1}\d+))\))/);
-
-// 				if(!results) return [0, 0, 0];
-// 				if(results[2] == '3d') return results.slice(2,5);
-
-// 				results.push(0);
-// 				return results.slice(6, 8); // returns the [X,Y,Z,1] values
-// 			}
-
-
-// 			var translation = getTransform( $m('#homepage_wrapper') );
-// 			var translationY = translation[0];
-// 			var absX = Math.abs(translationY);
-
-// 			alert(absX);
-//     });
-// });
-
 $(document).ready(function() {
 
 	//animated logo
@@ -55,3 +29,51 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function sliderPanelSize() {
+			$( ".fp-slidesNav li a span" ).css({
+					"height": $(window).height(),
+					"width": $(window).height()
+				});
+		}
+		$(window).on("load", sliderPanelSize);
+		$(window).on("resize", sliderPanelSize);
+
+		// Fullpage
+		$('#homepage_wrapper').fullpage({
+			anchors: ['main','product', 'outlet', 'showroom', 'premier', 'stories','contact'],
+			menu: '.navbar-nav',
+			fitToSection: false,
+			navigation: true,
+			navigationPosition: 'right',
+			slidesNavigation: true,
+			slidesNavPosition: "right",
+			controlArrows: true,
+			afterRender: function () {
+		        //on page load, start the slideshow
+		         slideTimeout = setInterval(function () {
+		                $.fn.fullpage.moveSlideRight();
+		            }, 8000);
+		    },
+		    onLeave: function (index, direction) {
+		        //after leaving section 1 (home) and going anywhere else, whether scrolling down to next section or clicking a nav link, this SHOULD stop the slideshow and allow you to navigate the site...but it does not
+		        if (index == '3') {
+		            console.log('on leaving the slideshow/section3');
+		            clearInterval(slideTimeout);
+		        }
+		   },
+			afterLoad: function(anchorLink, index){
+					if(index == 3){
+						$('.fadeInDown').addClass('animated');
+					}
+					if(index == 4){
+						$('.fadeInUp').addClass('animated');
+					}
+					if(index == 5){
+						$('.bounceInUp').addClass('animated');
+					}
+					if(index == 6){
+						$('.fadeInUp , .fadeInDown').addClass('animated');
+					}
+				}
+		});
